046deedb41eb604bfc32f49ae302af03528b3c16
Jacob Lindsley <jlindsley2@wisc.edu>
2018-06-12 21:33:19 -0500

    Update 21Sticks V2.0.0

--- 21sticks.js
+++ 21sticks.js
@@ -8,28 +8,33 @@ function newGame() {
 	numSticks = parseInt(prompt("Please input the number of sticks you would like to start with (12-100)"));
 	
 	while (isNaN(numSticks) || numSticks < 12 || numSticks > 100) {
-	numSticks = parseInt(prompt("Invalid input, please give a real number (12-100)"));
+		numSticks = parseInt(prompt("Invalid input, please give a real number (12-100)"));
 	}
 	
 	document.getElementById("lose").innerHTML =  "";
 	document.getElementById("win").innerHTML = "";
-	document.getElementById("numSticks").innerHTML = "";
+	document.getElementById("message").innerHTML = "";
 	update(0);
 }
 
 function update(numTaken) {
-	console.log("UPDATE: BOT HAS TAKEN " + numTaken + " STICKS.");
+	if (numTaken !== 0) {console.log("UPDATE: BOT HAS TAKEN " + numTaken + " STICKS.");}
 	numSticks -= numTaken;
 	if (numSticks <= 0) {
 		winGame();
 	}
-	document.getElementById("numSticks").innerHTML = '' + numSticks + " sticks remaining.";
-}
+	if (numTaken === 0) {
+		setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining</h3><br><br><br><br<br><br><br><br><br>";}, 1000);
+	} else if (numSticks != 0) {
+		setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining</h3><br><br> Opponent picked up: " + numTaken + "<br><br><br><br>";}, 1000);
+	}
+	}
 
 function bot(numTaken) {
 	console.log("Player picked up sticks: " + numTaken);
 	numSticks -= numTaken;
-	document.getElementById("numSticks").innerHTML = '' + numSticks + " sticks remaining.";
+	setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining </h3><br><br><br> You picked up: " + numTaken + "<br><br><br><br>";}, 10);
+	setTimeout(function() {
 	if (numSticks <= 0) {
 		loseGame();
 	} else if (numSticks === 1) {
@@ -44,13 +49,21 @@ function bot(numTaken) {
 		}
 	} else {
 		update(Math.floor(Math.random() * 3) + 1);
-	}
+	}}, 450);
 }
 
 function loseGame() {
-	document.getElementById("lose").innerHTML =  "You lost. The last stick was picked up by you. :(";
+	document.getElementById("message").innerHTML =  "You lost. The last stick was picked up by you. <br><br><br>:(<br><br><br>Want to try again?<br><br><br>";
 }
 
 function winGame() {
-	document.getElementById("win").innerHTML = "Congratulations, you beat the Stick Game AI!";
+	document.getElementById("message").innerHTML = "Congratulations, you beat the Stick Game AI!<br><br><br><br>Do you think you can win again?<br><br><br>";
+}
+
+function info() {
+	alert("The Game of Sticks (or 21 sticks), is a two player game. Each player takes turns picking up 1-3 sticks, and the player to pick up the last stick is the loser.(At least in this version of the game)");
+}
+
+function lose() {
+	alert("lol");
 }
