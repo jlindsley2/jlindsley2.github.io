fff81a8913a2c30dc996db69454a6697dd23a3a9
Jacob Lindsley <jlindsley2@wisc.edu>
2018-06-13 10:43:10 -0500

    21Sticks v2.1.0
    Disabled buttons while opponent takes turn. Added more info for buttons on the top. Disabled buttons after game ends. Replaced ascii man with a picture in order to scale. Added the words "YOU" and "OPPONENT" into pictures to scale with pictures. Centered game display on screen. Fixed scaling to look slightly better. Added version number to bottom of webpage.

--- 21sticks.js
+++ 21sticks.js
@@ -5,7 +5,11 @@ document.getElementById("take3").addEventListener("click", function() { bot(3); 
 
 
 function newGame() {
+	document.getElementById("game-board").style.display = "none";
+	document.getElementById("play-again").style.display = "none";
 	numSticks = parseInt(prompt("Please input the number of sticks you would like to start with (12-100)"));
+	document.getElementById("game-board").style.display = "inline";
+	document.getElementById("buttons").style.display = "inline";
 	
 	while (isNaN(numSticks) || numSticks < 12 || numSticks > 100) {
 		numSticks = parseInt(prompt("Invalid input, please give a real number (12-100)"));
@@ -19,25 +23,33 @@ function update(numTaken) {
 	if (numTaken !== 0) {console.log("UPDATE: BOT HAS TAKEN " + numTaken + " STICKS.");}
 	numSticks -= numTaken;
 	if (numSticks <= 0) {
-		winGame();
+		wonGame(true);
+		return;
 	}
 	if (numTaken === 0) {
 		setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining</h3><br><br><br><br<br><br><br><br><br>";}, 1000);
 	} else if (numSticks != 0) {
 		setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining</h3><br><br> Opponent picked up: " + numTaken + "<br><br><br><br>";}, 1000);
 	}
-	}
+}
 
 function bot(numTaken) {
 	console.log("Player picked up sticks: " + numTaken);
 	numSticks -= numTaken;
-	setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining </h3><br><br><br> You picked up: " + numTaken + "<br><br><br><br>";}, 10);
-	setTimeout(function() {
+	document.getElementById("buttons").style.display = "none";
 	if (numSticks <= 0) {
-		loseGame();
-	} else if (numSticks === 1) {
+		wonGame(false);
+		return;
+	}
+	
+	setTimeout(function() {document.getElementById("message").innerHTML = '<h3>' + numSticks + " sticks remaining </h3><br><br><br> You picked up: " + numTaken + "<br><br><br><br>";}, 25);
+	setTimeout(function() {
+	if (numSticks === 1) {
 		update(1);
-	} else if (numSticks < 5) {
+		return;
+	} 
+	setTimeout(function() {document.getElementById("buttons").style.display = "inline";}, 1500);
+	if (numSticks < 5) {
 		update(numSticks - 1);
 	} else if ((numSticks%4) - 1 !== 0) {
 		if ((numSticks%4) === 0) {
@@ -50,18 +62,24 @@ function bot(numTaken) {
 	}}, 450);
 }
 
-function loseGame() {
-	document.getElementById("message").innerHTML =  "You lost. The last stick was picked up by you. <br><br><br>:(<br><br><br>Want to try again?<br><br><br>";
-}
 
-function winGame() {
-	document.getElementById("message").innerHTML = "Congratulations, you beat the Stick Game AI!<br><br><br><br>Do you think you can win again?<br><br><br>";
+
+function wonGame(flag) {
+	console.log("wonGame called with: " + flag);
+	document.getElementById("buttons").style.display = "none";
+	
+	if (flag) {
+		document.getElementById("message").innerHTML = "Congratulations, you beat the Stick Game AI!<br><br><br><br>Do you think you can win again?<br><br><br>";
+	} else {
+		document.getElementById("message").innerHTML =  "You lost. The last stick was picked up by you. <br><br><br>:(<br><br><br>Want to try again?<br><br><br>";
+	}
+	document.getElementById("play-again").style.display = "inline";
 }
 
 function info() {
-	alert("The Game of Sticks (or 21 sticks), is a two player game. Each player takes turns picking up 1-3 sticks, and the player to pick up the last stick is the loser.(At least in this version of the game)");
+	alert("The Game of Sticks (or 21 sticks), is a two player game. Each player takes turns picking up 1-3 sticks, and the player to pick up the last stick is the loser. (At least in this version of the game)");
 }
 
 function lose() {
-	alert("lol");
+	alert("This was an idea I first learned about in discrete math a year ago. It was also the ONLY time since then that I've personally seen a practical use for mathematical induction. Essentially, the outcome of the game is determined by the first move.");
 }
